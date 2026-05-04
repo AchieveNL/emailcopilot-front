b83fd780018a75bd0ef13278156a96bf401a4f96
tretrak <3trak10@gmail.com>
2026-05-04 14:06:31 +0100

    fix api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
   headers: {
     "x-api-key": process.env.API_KEY || process.env.NEXT_PUBLIC_API_KEY || "",
     "Content-Type": "application/json",
