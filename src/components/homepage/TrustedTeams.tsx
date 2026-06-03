5cea675053c5a9d6fd089eb7ee1909e2c7db052d
RyanAwex <ryanawex@gmail.com>
2026-06-03 01:22:42 +0100

    add the carousel effect

--- src/components/homepage/TrustedTeams.tsx
+++ src/components/homepage/TrustedTeams.tsx
@@ -1,9 +1,22 @@
+"use client";
 import React from 'react'
 import ScrollFloat from './ScrollFloat'
+import LogoLoop from './LogoLoop'
+
+const TEAM_LOGOS = [
+  { src: "/logos/Vector.svg", alt: "Stripe" },
+  { src: "/logos/Group.svg", alt: "Shopify" },
+  { src: "/logos/Greenteam logo 1.svg", alt: "Airbnb" },
+  { src: "/logos/LOGO.svg", alt: "Netflix" },
+  { src: "/logos/Vector2.svg", alt: "Spotify" },
+  { src: "/logos/Sharkys 1.svg", alt: "Spotify" },
+  { src: "/logos/Layer_1.svg", alt: "Spotify" },
+  { src: "/logos/Vector3.svg", alt: "Spotify" },
+];
 
 function TrustedTeams() {
     return (
-        <section className='flex flex-col z-0 max-w-315 w-full gap-5 my-2'>
+        <section className='flex flex-col z-0 max-w-315 w-full gap-5 my-2 overflow-hidden'>
             {/* make the h2 element vertically centered between the two sections */}
             <ScrollFloat 
               containerClassName="text-2xl font-bold text-center mb-10 flex justify-center" 
@@ -11,17 +24,24 @@ function TrustedTeams() {
             >
               Trusted by growth-focused teams worldwide
             </ScrollFloat>
-            <div className="flex items-center justify-center gap-14 flex-wrap">
-                <img src="/logos/Vector.svg" alt="Stripe" className="h-6 grayscale opacity-80" />
-                <img src="/logos/Group.svg" alt="Shopify" className="h-6 grayscale opacity-80" />
-                <img src="/logos/Greenteam logo 1.svg" alt="Airbnb" className="h-6 grayscale opacity-80" />
-                <img src="/logos/LOGO.svg" alt="Netflix" className="h-6 grayscale opacity-80" />
-                <img src="/logos/Vector2.svg" alt="Spotify" className="h-6 grayscale opacity-80" />
-                <img src="/logos/Sharkys 1.svg" alt="Spotify" className="h-6 grayscale opacity-80" />
-                <img src="/logos/Layer_1.svg" alt="Spotify" className="h-6 grayscale opacity-80" />
-                <img src="/logos/Vector3.svg" alt="Spotify" className="h-6 grayscale opacity-80" />
-            </div>
-
+            <LogoLoop
+              logos={TEAM_LOGOS}
+              logoHeight={24}
+              gap={64}
+              speed={50}
+              fadeOut={true}
+              fadeOutColor="#fafafa"
+              renderItem={(item) => {
+                const logo = item as any;
+                return (
+                  <img
+                    src={logo.src}
+                    alt={logo.alt}
+                    className="h-6 grayscale opacity-80"
+                  />
+                );
+              }}
+            />
         </section>
     )
 }
